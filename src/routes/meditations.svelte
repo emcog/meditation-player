95f00ca7b49d317c0a2a530ce9292d0142664402
Robin <robin.leonard@gmail.com>
2022-07-09 21:25:57 +0100

    added meditations info

---
+++ src/routes/meditations.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+</script>
+<div>Audio</div>
+<audio
+	controls
+	src='https://social-mindfulness-meditations.s3.eu-west-2.amazonaws.com/podcasts/bodyscan.mp3'>
+</audio>
+<table>
+	<tr>
+		<td>01</td><td>Name</td><td>duration</td>
+	</tr>
+</table>
+<style>
+
+</style>
---
+++ static/meditationsData.js
@@ -0,0 +1,30 @@
+export const meditationsData = [
+	{
+		title: "Three-Step reset",
+		url: " https://media.mindfulnessconnected.com/three-step-reset.mp3"
+	},
+	{
+		title: "Bodyscan",
+		url: "https://media.mindfulnessconnected.com/bodyscan.mp33"
+	},
+	{
+		title: "Bodyscan",
+		url: "https://media.mindfulnessconnected.com/bodyscan.mp33"
+	},
+	{
+		title: "Posture and breath",
+		url: "https://media.mindfulnessconnected.com/posture-and-breath.mp3"
+	},
+	{
+		title: "Stretching and sitting",
+		url: "https://media.mindfulnessconnected.com/stretching-and-sitting.mp3"
+	},
+	{
+		title: "Mindfulness of feelings",
+		url: "https://media.mindfulnessconnected.com/mindfulness-of-feelings.mp3"
+	},
+	{
+		title: "Concentration and allowing the mind to wander",
+		url: "https://media.mindfulnessconnected.com/concentration-and-wandering-mind.mp3"
+	}
+]
