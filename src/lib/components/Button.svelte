eb2e0069b0cef022f51a01f5dc10a48dafc75185
Robin <robin.leonard@gmail.com>
2022-07-16 21:14:07 +0100

    added type-scale stlyes

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -2,19 +2,6 @@
 	export let cta;
 	export let parentClass;
 </script>
-<button class='{parentClass}'>{cta}</button>
-<style>
-		.primary {
-
-		}
-		.secondary {
-			background: white;
-			border-color: black;
-		}
-	button {
-			border-radius: 9999px;
-			border-width: 2px;
-			text-transform: uppercase;
-
-	}
-</style>
+<button class='{parentClass} rounded-full border-2 uppercase'>
+	{cta}
+</button>
