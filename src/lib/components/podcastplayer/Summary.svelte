1cea8d121fa63b9224cfbf56e244fc36acb842f1
Robin <robin.leonard@gmail.com>
2022-07-17 22:34:27 +0100

    added iframe toggle functionality

--- src/lib/components/podcastplayer/Summary.svelte
+++ src/lib/components/podcastplayer/Summary.svelte
@@ -1,7 +1,22 @@
 <script>
 	import { summary } from '$lib/data/podcast.json';
+
+	let isIframeVisible = false;
+
+	function handleClick() {
+		return isIframeVisible = true;
+	}
+
 </script>
 <div class="relative">
-	<p class="[height:70px] [text-overflow: ellipsis] [overflow:hidden]" aria-label="summary">{summary}</p>
-	<button class="uppercase absolute top-[33px] right-0 pl-16 bg-gradient-to-l from-white via-white">more</button>
+	<p class="[height:70px] [text-overflow: ellipsis] [overflow:hidden]" aria-label="truncated summary">{summary}</p>
+	<button on:click={handleClick} class="uppercase absolute top-[33px] right-0 pl-16 bg-gradient-to-l from-white via-white">more</button>
+	<iframe class:toggleIframe={isIframeVisible === false} title="Full summary" width="300"
+					height="200" srcdoc='<html><body>{summary}</body></html>'></iframe>
 </div>
+
+<style>
+	.toggleIframe {
+			visibility: hidden;
+	}
+</style>
