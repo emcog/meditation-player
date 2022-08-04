935386b3d60316920dcd20d92ffdd5d245a34941
Robin <robin.leonard@gmail.com>
2022-08-04 08:28:01 +0100

    more/less button working

--- src/lib/components/podcastplayer/Podcast.svelte
+++ src/lib/components/podcastplayer/Podcast.svelte
@@ -1,11 +1,30 @@
 <script>
-	import { title } from '$lib/data/podcast.json';
-	import Summary from './Summary.svelte';
+	import { title, description } from '$lib/data/podcast.json';
 	export let podcastClass;
+
+	// https://stackoverflow.com/questions/18146354/how-can-i-cut-a-string-after-x-characters#18146415
+	function truncateText(text, length) {
+		if (text.length <= length) {
+			return text;
+		}
+		return text.substr(0, length) + '\u2026'
+	}
+
+	// find length of text, when more button === true run trunCate text with length of text
+	let shortText = 150;
+	let bodyCopy = truncateText;
+	let fullText = description.length;
+	let moreText = false
+
+	function toggleMoreText(){
+		moreText ? moreText = false : moreText = true;
+	}
+
+
 </script>
 <section class="{podcastClass} border-b-2 pb-6 mb-1" aria-label='summary' >
 	<img class="row-span-1 rounded drop-shadow-md"
-			 alt="'I relate therefore I am', A portrait of Descartes with a images of people overlaid"
+			 alt="'I relate therefore I am', A portrait of Descartes with a semi-transparent grid of people on top"
 			 src="$lib/images/MBOE-social-mindfulness_cover-1x.jpeg"
 			 srcset="src/lib/images/MBOE-social-mindfulness_cover-1x.jpeg 1x,
 								src/lib/images/MBOE-social-mindfulness_cover-1halfx.jpeg 1.5x,
@@ -14,11 +33,8 @@
 								src/lib/images/MBOE-social-mindfulness_cover-4x.jpeg 4x" />
 	<div class="row-span-2 -mt-3">
 		<h1>{title}</h1>
-		<p>mobile grid</p>
-		<p>change i frame to expand/collapse </p>
-<!--		<p aria-label="sub-title">{subtitle}</p>-->
-		<Summary/>
-
+		<p class="">{moreText ? bodyCopy(description, fullText) : bodyCopy(description, shortText)}</p>
+		<button class="float-right underline {moreText? '-mt-2':'mt-[-54px]'}" on:click='{toggleMoreText}'>{moreText? "Less":"More"}</button>
 	</div>
 </section>
 <style>
