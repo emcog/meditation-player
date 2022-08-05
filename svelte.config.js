40ac7a2650e6605f9e81e97843e52d313db14693
Robin <robin.leonard@gmail.com>
2022-08-05 10:57:24 +0100

    revert to default generic config

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,18 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+
 		prerender: {
-		default: true
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
 		}
 	}
 };
