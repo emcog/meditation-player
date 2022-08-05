5fb465d5ef8c7e69d40ea5f97172aa3390fb4923
Robin <robin.leonard@gmail.com>
2022-08-05 12:21:16 +0100

    changed location of images

--- src/lib/components/podcastplayer/Podcast.svelte
+++ src/lib/components/podcastplayer/Podcast.svelte
@@ -25,12 +25,10 @@
 <section class="{podcastClass} border-b-2 pb-6 mb-1" aria-label='summary' >
 	<img class="row-span-1 rounded drop-shadow-md"
 			 alt="'I relate therefore I am', A portrait of Descartes with a semi-transparent grid of people on top"
-			 src="$lib/images/MBOE-social-mindfulness_cover-1x.jpeg"
-			 srcset="src/lib/images/MBOE-social-mindfulness_cover-1x.jpeg 1x,
-								src/lib/images/MBOE-social-mindfulness_cover-1halfx.jpeg 1.5x,
-								src/lib/images/MBOE-social-mindfulness_cover-2x.jpeg 2x,
-								src/lib/images/MBOE-social-mindfulness_cover-3x.jpeg 3x,
-								src/lib/images/MBOE-social-mindfulness_cover-4x.jpeg 4x" />
+			 src="/MBOE-social-mindfulness_cover-1x.jpeg"
+			 srcset='/MBOE-social-mindfulness_cover-1x.jpeg 1x,
+								/MBOE-social-mindfulness_cover-1halfx.jpeg 1.5x,
+/MBOE-social-mindfulness_cover-2x.jpeg-2x.jpeg ../../../../static/MBOE-social-mindfulness_cover-3x.jpegover-3x.j../../../../static/MBOE-social-mindfulness_cover-4x.jpegss_cover-4x.jpeg 4x' />
 	<div class="row-span-2 mt-3 md:-mt-3">
 		<h1>{title}</h1>
 		<p class="">{moreText ? bodyCopy(description, fullText) : bodyCopy(description, shortText)}</p>
